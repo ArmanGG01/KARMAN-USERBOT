81b7067538aababfd9b6f8a0e4b21417ffb932f5
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-21 09:00:59 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,9 @@
-"" Userbot start point """
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
 
 from importlib import import_module
 from sys import argv
