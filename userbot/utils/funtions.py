f180d4b52706fe2eceb75dd9afb40886d6206b46
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-21 09:50:03 +0700

    Update funtions.py

--- userbot/utils/funtions.py
+++ userbot/utils/funtions.py
@@ -3,6 +3,7 @@ import shlex
 import asyncio
 from os.path import basename
 from typing import Optional, Tuple
+from youtubesearchpython import VideosSearch
 
 # For using gif , animated stickers and videos in some parts , this
 # function takes  take a screenshot and stores ported from userge
@@ -22,7 +23,8 @@ async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Op
         print(err)
     return thumb_image_path if os.path.exists(thumb_image_path) else None
 
-# executing of terminal commands
+
+# Executing of terminal commands
 
 
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
@@ -35,3 +37,45 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
             stderr.decode('utf-8', 'replace').strip(),
             process.returncode,
             process.pid)
+
+
+async def ytsearch(query, limit):
+    result = ""
+    videolinks = VideosSearch(query.lower(), limit=limit)
+    for v in videolinks.result()["result"]:
+        textresult = f"[{v['title']}](https://www.youtube.com/watch?v={v['id']})\n"
+        try:
+            textresult += f"**Description : **`{v['descriptionSnippet'][-1]['text']}`\n"
+        except Exception:
+            textresult += "**Description : **`None`\n"
+        textresult += f"**Duration : **__{v['duration']}__  **Views : **__{v['viewCount']['short']}__\n"
+        result += f"☞ {textresult}\n"
+    return result
+
+
+# https://github.com/pokurt/LyndaRobot/blob/7556ca0efafd357008131fa88401a8bb8057006f/lynda/modules/helper_funcs/string_handling.py#L238
+
+
+async def extract_time(cat, time_val):
+    if any(time_val.endswith(unit) for unit in ("m", "h", "d", "w")):
+        unit = time_val[-1]
+        time_num = time_val[:-1]  # type: str
+        if not time_num.isdigit():
+            cat.edit("Invalid time amount specified.")
+            return ""
+        if unit == "m":
+            bantime = int(time.time() + int(time_num) * 60)
+        elif unit == "h":
+            bantime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == "d":
+            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        elif unit == "w":
+            bantime = int(time.time() + int(time_num) * 7 * 24 * 60 * 60)
+        else:
+            # how even...?
+            return ""
+        return bantime
+    await cat.edit(
+        f"Invalid time type specified. Expected m , h , d or w but got: {time_val[-1]}"
+    )
+    return ""
