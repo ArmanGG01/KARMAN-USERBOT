9d9b435ce67b0dcb65ba1db911a194c15ce6f801
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-21 09:20:45 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -16,5 +16,5 @@
 
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
-from .progress import progress
+from .progress import 'progress
 from .tools import humanbytes, time_formatter, human_to_bytes, md5, take_screen_shot, runcmd
