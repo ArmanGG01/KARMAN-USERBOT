ce4308dcc1f8de19ac2c71b98c3062de0666b8c3
Landak <80225366+ramadhani892@users.noreply.github.com>
2021-08-07 06:35:49 +0700

    Update ramfaction.py

--- userbot/modules/ramfaction.py
+++ userbot/modules/ramfaction.py
@@ -6,8 +6,8 @@ import asyncio
 
 
 @register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
-async def _(event):
-    t = event.pattern_match.group(1)
+async def _(landak):
+    t = landak.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -15,18 +15,18 @@ async def _(event):
             t = int(t)
         except BaseException:
             try:
-                t = await event.ban_time(t)
+                t = await landak.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Kebanyakan fake hidup lu ngentot!`")
-    await event.edit(f"`Memulai Fake Typing {t} detik.`")
-    async with event.client.action(event.chat_id, "typing"):
+                return await landak.edit("`Incorrect Format`")
+    await landak.edit(f"`Kebanyakan fake hidup lu ngentot!`")
+    await landak.edit(f"`Memulai Fake Typing {t} detik.`")
+    async with landak.client.action(landak.chat_id, "typing"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.faudio(?: |$)(.*)")
-async def _(event):
-    t = event.pattern_match.group(1)
+async def _(landak):
+    t = landak.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -34,17 +34,17 @@ async def _(event):
             t = int(t)
         except BaseException:
             try:
-                t = await event.ban_time(t)
+                t = await landak.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai Fake Audio dalam {t} detik.`")
-    async with event.client.action(event.chat_id, "record-audio"):
+                return await landak.edit("`Incorrect Format`")
+    await landak.edit(f"`Memulai Fake Audio dalam {t} detik.`")
+    async with landak.client.action(landak.chat_id, "record-audio"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.fvideo(?: |$)(.*)")
-async def _(event):
-    t = event.pattern_match.group(1)
+async def _(landak):
+    t = landak.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -52,17 +52,17 @@ async def _(event):
             t = int(t)
         except BaseException:
             try:
-                t = await event.ban_time(t)
+                t = await landak.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai Fake video dalam {t} detik.`")
-    async with event.client.action(event.chat_id, "record-video"):
+                return await landak.edit("`Incorrect Format`")
+    await landak.edit(f"`Memulai Fake video dalam {t} detik.`")
+    async with landak.client.action(landak.chat_id, "record-video"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.fgame(?: |$)(.*)")
-async def _(event):
-    t = event.pattern_match.group(1)
+async def _(landak):
+    t = landak.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -70,11 +70,11 @@ async def _(event):
             t = int(t)
         except BaseException:
             try:
-                t = await event.ban_time(t)
+                t = await landak.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai Fake Play game dalam {t} detik.`")
-    async with event.client.action(event.chat_id, "game"):
+                return await landak.edit("`Incorrect Format`")
+    await landak.edit(f"`Memulai Fake Play game dalam {t} detik.`")
+    async with landak.client.action(landak.chat_id, "game"):
         await asyncio.sleep(t)
 
 CMD_HELP.update(
