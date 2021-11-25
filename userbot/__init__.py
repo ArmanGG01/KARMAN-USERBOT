3725786ad97010f13739d361ff1245b5c48e0a93
Landak <80225366+ramadhani892@users.noreply.github.com>
2021-11-26 01:18:55 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -540,7 +540,7 @@ with bot:
                 result = builder.photo(
                     file=ramlogo,
                     link_preview=False,
-                    text=f"**✨ inline RAM-UBOT ✨**\n\n⭐ **Owner** [LANDAK](t.me/maafgausahsokap)\n✨ **Jumlah** `{len(plugins)}` Modules" .format(
+                    text=f"**✨ inline RAM-UBOT ✨**\n\n⭐ **Owner** [LANDAK](t.me/maafgausahsokap)\n✨ **Jumlah** `{len(plugins)}` Modules\n✨ **Support** [GEEZ SUPPORT](t.me/geezsupportgroup) ".format(
                         len(dugmeler),
                     ),
                     buttons=buttons,
