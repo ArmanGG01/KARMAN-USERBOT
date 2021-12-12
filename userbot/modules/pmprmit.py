14da1b041156c844d85245d8498fbb63686fd40e
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2021-12-13 02:00:00 +0700

    Update pmprmit.py

--- userbot/modules/pmprmit.py
+++ userbot/modules/pmprmit.py
@@ -28,14 +28,14 @@ from userbot.events import register
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
 DEF_UNAPPROVED_MSG = (
-    f"**╭━━━━━━━━━━━━━━━━━╮**\n    🌟SELAMAT DATANG🌟\n"
+    f"**╭━━━━━━━━━━━━━━━━━╮**\n    💀SELAMAT DATANG💀\n"
     "**╰━━━━━━━━━━━━━━━━━╯**\n"
     "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
-    f"**ANAK KONTOL ANAK NGENTOT,KALO NGECHAT MAJIKAN {DEFAULTUSER} ITU SALAM,\nHABIS ITU SABAR TUNGGU MAJIKAN GUA BALES,\nKALO GA DI BALES - BALES, LU JANGAN NYEPAM KONTOL, KAYA ANAK YATIM MAU MINTA SEMBAKO LU ANJING, APA LAGI LU NGECHAT NYA CUMA MINTA VCS, BISA GUA BLOKIR!! KALO NYEPAM JUGA TAR GUA BLOKIR!!!! TUNGGU SI {DEFAULTUSER} NERIMA PESAN LU**\n"
+    f"**WOY BABI TUNGGU SAMPEK MAJIKAN KU NGEBALS {DEFAULTUSER} UTAMIIN UCAPIN SALAM,\nHABIS ITU SABAR TUNGGU MAJIKAN KU  NGEBALES,\nKALO GA DI BALES - BALES, JANGAN DI SAPAM, KAYA ANAK YATIM MAU MINTA SEMBAKO LU ANJING, APA LAGI LU NGECHAT NYA CUMA MINTA VCS, BISA GUA BLOKIR!! KALO NYEPAM JUGA TAR GUA BLOKIR!!!! TUNGGU SI {DEFAULTUSER} NERIMA PESAN LU**\n"
     "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
     "╭✠╼━━━━━━❖━━━━━━━✠╮\n"
     "┣[• 𝐁𝐎𝐓 𝐌𝐄𝐒𝐒𝐀𝐆𝐄\n"
-    "┣[• 𝐁𝐘 ⭐️𝐑𝐀𝐌-𝐔𝐁𝐎𝐓⭐️\n"
+    "┣[• 𝐁𝐘 💀𝐊𝐀𝐑𝐌𝐀𝐍💀\n"
     "╰✠╼━━━━━━❖━━━━━━━✠╯")
 # =================================================================
 
