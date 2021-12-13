c196ab355ee34f061e056291c74757dcc34c56ce
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2021-12-13 16:39:42 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -662,7 +662,7 @@ with bot:
             )
         )
         async def on_plug_in_callback_query_handler(event):
-            if event.query.user_id == uid:  # @Ram_ubot
+            if event.query.user_id == uid:  # @KarmanNewuser_bot
                 # https://t.me/TelethonChat/115200
                 await event.edit(
                     file=ramlogo,
