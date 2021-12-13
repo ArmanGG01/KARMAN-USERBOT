8cbf6d744bae7d37935c1877d789e274687fddad
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2021-12-13 14:26:50 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"💀KARMAN-USERBOT💀 V7.0 [AKTIF NGENTOT!!!!]")
+    f"💀KARMAN-USERBOT💀 V7.0 [AKTIF KONTOLL!]")
 
 
 if len(argv) not in (1, 3, 4):
