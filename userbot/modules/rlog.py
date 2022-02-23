ac01857bfc6cd98331176344e53d36b080c5fd1f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-02-23 17:46:15 +0700

    Update rlog.py

--- userbot/modules/rlog.py
+++ userbot/modules/rlog.py
@@ -8,7 +8,7 @@ from telethon import events
 
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HELP, LOGS, bot
-from userbot.modules.sql_helper import no_log_pms_sql
+from userbot.modules.sql_helper import no_log_pms_sql 
 from userbot.modules.sql_helper.globals import addgvar, gvarstatus
 from userbot.modules.ramcals import vcmention
 from userbot.utils import _format
