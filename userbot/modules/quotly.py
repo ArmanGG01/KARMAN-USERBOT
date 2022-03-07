b50cedd24af5b584ac5202771053f9cb2ac648d6
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-07 20:52:50 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -14,7 +14,7 @@ from userbot.utils import edit_delete, edit_or_reply, ram_cmd
 from userbot.utils.misc import create_quotly
 from telethon.tl.functions.users import GetFullUserRequest
 
-from .karmancarbon import all_col
+from .ramcarbon import all_col
 
 
 @rman_cmd(pattern="\.q ?(.*)")
