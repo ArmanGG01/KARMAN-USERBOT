9bb300fa8e0af4d98bffeff72e67c6552ad93c27
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-08 11:16:37 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -32,7 +32,7 @@ from telethon.sync import TelegramClient, custom, events
 from telethon.sessions import StringSession
 from telethon import Button, events, functions, types
 from telethon.utils import get_display_name
-from userbot import CMD_HANDLER 
+from userbot import CMD_HANDLER as cmd
 
 redis_db = None
 
