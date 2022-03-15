4425bfb02e336847a4ec082f285fcf7358983c1c
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-15 14:26:58 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -131,7 +131,7 @@ async def redis(pong):
 
 
 @register(outgoing=True, pattern="^Ping$")
-async def redis(pong):
+async def _(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
