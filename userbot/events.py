88f390b180e4b460e749a5c99751147ff82340e8
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-20 18:43:27 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -187,7 +187,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ COBA-UBOT ERROR REPORT ✘**\n\n"
+                    text = "**✘ KARMAN-USERBOT ERROR REPORT ✘**\n\n"
                     link = "[Group Support](https://t.me/obrolansuar)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
