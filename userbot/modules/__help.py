7c804f736911529b5be6d3e8da2ca7d7561f2829
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-20 18:31:01 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,10 +1,10 @@
 # Copyright (C) 2020 TeamDerUntergang.
-#
+###
 # SedenUserBot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# Rama ganteng
+#
 # SedenUserBot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,32 +16,60 @@
 # @Qulec tarafından yazılmıştır.
 # Thanks @Spechide.
 
-import logging
-
+from telethon.errors.rpcerrorlist import BotInlineDisabledError as noinline
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import BOT_USERNAME
-from userbot.events import register
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
+from userbot import CMD_HANDLER as cmd
+from userbot import bot
+from userbot.utils import edit_or_reply, kar_cmd
 
 
-@register(outgoing=True, pattern=r"^\.rhelp")
-async def yardim(event):
-    try:
-        tgbotusername = BOT_USERNAME
-        if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@KarmanNewuser_bot")
+@kar_cmd(pattern="rhelp")
+async def _(event):
+    if event.fwd_from:
+        return
+    if BOT_USERNAME is not None:
+        chat = "@Botfather"
+        try:
+            results = await event.client.inline_query(BOT_USERNAME, "@KarmanNewuser_bot")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-        else:
-            await event.edit(
-                "`Bot tidak berfungsi! Harap setel Token Bot dan Nama Pengguna dengan benar. Modul telah dihentikan.`"
+        except noinline:
+            xx = await edit_or_reply(
+                event,
+                "**Inline Mode Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
             )
-    except Exception:
-        return await event.edit(
-            "`Anda tidak dapat Menggunakan Perintah .rhelp, Bisa Jadi Grup Ini tidak Mensupport nya (Mungkin Anda Belum Menghidupakan Inline Mode Dan Inline Location Data)`"
+            async with bot.conversation(chat) as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(BOT_USERNAME)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message("Search")
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    await event.client(UnblockRequest(chat))
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(BOT_USERNAME)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message("Search")
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                await xx.edit(
+                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}rhelp` **lagi untuk membuka menu bantuan.**"
+                )
+            await bot.delete_messages(
+                conv.chat_id,
+                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
+            )
+    else:
+        await edit_or_reply(
+            event,
+            "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
         )
