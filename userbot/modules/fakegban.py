2f50b5bd177d9b26000114f4ae3c0f3563cf231b
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-22 13:47:04 +0700

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -1,30 +1,26 @@
 # This is a troll indeed ffs *facepalm*
 # Ported from xtra-telegram by @heyworld
 import asyncio
+
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
-#from userbot.utils import admin_cmd
-from userbot.events import register
-from userbot import ALIVE_NAME, CMD_HELP, DEVS, bot
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot, owner
+from userbot.utils import edit_or_reply, kar_cmd
 
 
-@register(outgoing=True, pattern="^.fgban(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS, pattern=r"^\.cgbn(?: |$)")
+@kar_cmd(pattern="fgban(?: |$)(.*)")
 async def gbun(event):
     if event.fwd_from:
         return
     gbunVar = event.text
     gbunVar = gbunVar[6:]
-    mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
-    no_reason = "No Reason Given "
-    await event.edit("**Summoning out the mighty gban hammer ☠️**")
+    mentions = f"**Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By** {owner}\n"
+    await edit_or_reply(event, "**Summoning out the mighty gban hammer ☠️**")
     asyncio.sleep(3.5)
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
+    async for _ in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
     reply_message = None
     if event.reply_to_msg_id:
@@ -34,15 +30,18 @@ async def gbun(event):
         usname = replied_user.user.username
         idd = reply_message.from_id
         # make meself invulnerable cuz why not xD
-        if idd == 1036951071:
-            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Heyworld](tg://user?id=1036951071) __to release your account__😏")
+        if idd == 5155140917:
+            await reply_message.reply(
+                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Heyworld](tg://user?id=5155140917) __to release your account__😏"
+            )
         else:
-            jnl = ("`Warning!!`"
-                   "[{}](tg://user?id={})"
-                   f"` 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n\n"
-                   "**Name: ** __{}__\n"
-                   "**ID : ** `{}`\n"
-                   ).format(firstname, idd, firstname, idd)
+            jnl = (
+                "**Warning!!**"
+                "[{}](tg://user?id={})"
+                f"** 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By** {owner}\n\n"
+                "**Name: ** __{}__\n"
+                "**ID : ** `{}`\n"
+            ).format(firstname, idd, firstname, idd)
             if usname is None:
                 jnl += "**Username: ** `Doesn't own a username!`\n"
             elif usname != "None":
@@ -52,15 +51,20 @@ async def gbun(event):
                 gbunr = "**Reason: **" + gbunm
                 jnl += gbunr
             else:
+                no_reason = "**Reason: **`null`"
                 jnl += no_reason
             await reply_message.reply(jnl)
     else:
-        mention = (
-            f"Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By {DEFAULTUSER} \nReason: No Reason Given. ")
+        mention = f"**Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By** {owner} \n**Reason:** `null` "
         await event.reply(mention)
     await event.delete()
 
-CMD_HELP.update({
-    "fakegban": "`.fgban`\
-    \nUsage: Type .fgban or Reply .fgban reason and see it yourself. "
-})
+
+CMD_HELP.update(
+    {
+        "fakegban": f"**Plugin : **`fakegban`\
+        \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}fgban` <reply> <reason>\
+        \n  ❍▸ : **Untuk melakukan aksi Fake global banned , just for fun\
+    "
+    }
+)
