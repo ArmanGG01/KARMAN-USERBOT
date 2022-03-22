b6cfe494c120e39716dd1764d1c8a5bf4bc031f2
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-03-22 15:25:04 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -43,7 +43,7 @@ async def autobot():
     if who.username:
         username = who.username + "_ubot"
     else:
-        username = "geez" + (str(who.id))[5:] + "ubot"
+        username = "karman" + (str(who.id))[5:] + "ubot"
     bf = "@BotFather"
     await bot(UnblockRequest(bf))
     await bot.send_message(bf, "/cancel")
