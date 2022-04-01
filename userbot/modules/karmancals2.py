ef30df8804bf3754a27b80e7eb46cc94e4b5be9b
sᴋʏ <92210483+Cangcimenn@users.noreply.github.com>
2022-04-01 15:39:10 +0700

    Update karmancals2.py

--- userbot/modules/karmancals2.py
+++ userbot/modules/karmancals2.py
@@ -12,12 +12,6 @@ from pytgcalls.types.input_stream.quality import (
     LowQualityVideo,
     MediumQualityVideo,
 )
-from pytgcalls.exceptions import (
-    NoActiveGroupCall as memek,
-    AlreadyJoinedError as asu,
-    NotInGroupCallError as ajg
-)
-
 from telethon.tl import types
 from telethon.utils import get_display_name
 from youtubesearchpython import VideosSearch
@@ -455,60 +449,6 @@ async def vc_volume(event):
             await edit_delete(event, f"**ERROR:** `{e}`", 30)
     else:
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
-
-
-# credits by @vckyaz < vicky \>
-# FROM GeezProjects < https://github.com/vckyou/GeezProjects \>
-# ambil boleh apus credits jangan ya ka:)
-# Thanks recode RAM-UBOT
-
-
-@kar_cmd(pattern="joinvc(?: |$)(.*)")
-@tod(pattern=r"^\.cjoinvc(?: |$)(.*)")
-async def join_(event):
-    await edit_or_reply(event, f"**Procesing....**")
-    if len(event.text.split()) > 1:
-        chat = event.chat_id
-        chats = event.pattern_match.group(1)
-        try:
-            chat = await event.client(GetFullUserRequest(chats))
-        except asu as e:
-            await call_py.leave_group_call(chat)
-            clear_queue(chat)
-            await asyncio.sleep(3)
-            return await edit_delete(event, f"**ERROR:** `{e}`", 30)
-        except (NodeJSNotInstalled, TooOldNodeJSVersion):
-            return await edit_or_reply(event, "NodeJs is not installed or installed version is too old.")
-    else:
-        chat_id = event.chat_id
-        chats = event.pattern_match.group(1)
-        vcmention(event.sender)
-    if not call_py.is_connected:
-        await call_py.start()
-    await call_py.join_group_call(
-        chat_id,
-        rambot(
-            'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
-        ),
-        chats,
-        stream_type=ya().pulse_stream,
-    )
-    await edit_delete(event, f"**Berhasil Join Obrolan Suara.**\n**ID:{chat_id}!**", 5)
-
-
-@kar_cmd(pattern="leavevc(?: |$)(.*)")
-@tod(pattern=r"^\.cleavevc(?: |$)(.*)")
-async def leavevc(event):
-    """ leave video chat """
-    await edit_or_reply(event, "**Processing....**")
-    chat_id = event.chat_id
-    from_user = vcmention(event.sender)
-    if from_user:
-        try:
-            await call_py.leave_group_call(chat_id)
-        except (memek, ajg):
-            await edit_or_reply(event, f"Eh {from_user}, Lo ga ada di os ngentot!!!!!")
-        await edit_delete(event, f"**Babay Anak kontol, {from_user} Turun dulu...**", 2)
         
         
 @kar_cmd(pattern="playlist$")
