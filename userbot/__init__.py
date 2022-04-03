cae4349f0106e723b441a928ff7675acca865e79
sᴋʏ <92210483+Cangcimenn@users.noreply.github.com>
2022-04-03 15:50:47 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -90,7 +90,7 @@ API_HASH = os.environ.get("API_HASH", "")
 STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Logging channel/group ID configuration.
-BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID") or 0)
+BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", ""))
 
 # Userbot logging feature switch.
 BOTLOG = sb(os.environ.get("BOTLOG", "True"))
