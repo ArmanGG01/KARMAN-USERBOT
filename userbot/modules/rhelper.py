09d1bc77ca4845ad5e5e1c8b0f13e34c8172c719
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-06-24 17:57:13 +0700

    Update rhelper.py

--- userbot/modules/rhelper.py
+++ userbot/modules/rhelper.py
@@ -13,9 +13,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
-        "\n[ARMAN](t.me/masukksiniJKN)"
+        "\n[ARMAN](t.me/PakkPoll)"
         "\n\n[SUPPORT](https://t.me/obrolansuar)"
-        "\n\n[CHANNEL](https://t.me/DeployBot01")
+        "\n\n[CHANNEL](https://t.me/StoryArman")
 
 
 @register(outgoing=True, pattern="^.rvars$")
