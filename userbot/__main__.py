9e787e108891c60cb279d46bab80a31b7b6f60ea
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-22 14:51:12 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,58 +1,36 @@
-## Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2019 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot start point """
 
-import sys
 from importlib import import_module
-from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, LOGS, bot, call_py, karblacklist
-from userbot.modules import ALL_MODULES
-from userbot.utils.utils import autobot
-from userbot.utils.tools import hadeh_ajg
+from sys import argv
 
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from userbot import BOT_VER, LOGS, bot
+from userbot.modules import ALL_MODULES
+
+INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
+             '\n Tip: Use Country Code along with number.' \
+             '\n or check your phone number and try again !'
 
 try:
-    for module_name in ALL_MODULES:
-        imported_module = import_module("userbot.modules." + module_name)
     bot.start()
-    call_py.start()
-    user = bot.get_me()
-    if user.id in karblacklist:
-        LOGS.warning(
-            "MAKANYA GA USAH BANYAK TINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK KAU.\nCredits: @PakkPoll"
-        )
-        sys.exit(1)
-    LOGS.info(f"💀KARMAN - USERBOT💀 🌟 V{9.0} [ TELAH AKTIFKAN KONTOL ]")
-except BaseException as e:
-    LOGS.info(str(e), exc_info=True)
-    sys.exit(1)
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
 
 
-
-async def ram_ubot_on():
-    try:
-        if BOTLOG_CHATID != 0:
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"{BOTLOG_MSG}",
-            )
-    except Exception as e:
-        LOGS.info(str(e))
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
+LOGS.info(
+    f"💀KARMAN-USERBOT💀 V7.0 [AKTIF KONTOLL!]")
 
 
-bot.loop.run_until_complete(ram_ubot_on())
-bot.loop.run_until_complete(autobot())
-idle()
-bot.loop.run_until_complete(hadeh_ajg())
-if len(sys.argv) not in (1, 3, 4):
+if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
